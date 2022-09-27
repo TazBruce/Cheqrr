c7257e2256da7697f6de451d4b4df19ad643fc28
Tasman Bruce <tasmanbrucework@gmail.com>
2022-09-27 14:03:38 +1300

    Register functionality

--- frontend/src/pages/loggedOut/RegisterPage.vue
+++ frontend/src/pages/loggedOut/RegisterPage.vue
@@ -14,16 +14,34 @@
             v-model="password"
             label="Password"
             filled
+            :type="isPwd ? 'password' : 'text'"
             lazy-rules
-            :rules="[val => val && val.length > 0 || 'Please type your password']"
-          />
+            :rules="[val => val && val.length > 0 || 'Please type your password',
+                     val => val.length > 7 || 'Password must be at least 8 characters']">
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
           <q-input
             v-model="passwordConfirmation"
             label="Password Confirmation"
             filled
+            :type="isPwd ? 'password' : 'text'"
             lazy-rules
-            :rules="[val => val && val.length > 0 || 'Please type your password confirmation']"
-          />
+            :rules="[val => val && val.length > 0 || 'Please type your password confirmation',
+                     val => val.length > 7 || 'Password must be at least 8 characters']">
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
           <q-btn label="Register" type="submit" color="primary" />
           <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
         </q-form>
@@ -34,10 +52,14 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+import { useAuth } from '../../stores/auth.store';
+
 export default {
   name: 'RegisterPage',
   data () {
     return {
+      isPwd: ref(true),
       email: '',
       password: '',
       passwordConfirmation: '',
@@ -45,11 +67,12 @@ export default {
   },
   methods: {
     onSubmit () {
-      this.$store.dispatch('auth/register', {
-        email: this.email,
-        password: this.password,
-        password_confirmation: this.passwordConfirmation,
-      })
+      const auth = useAuth();
+      if (this.password !== this.passwordConfirmation) {
+        alert('Password and password confirmation do not match');
+      } else {
+        auth.register(this.email, this.password);
+      }
     },
     onReset () {
       this.email = ''
