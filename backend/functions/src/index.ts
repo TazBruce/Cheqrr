d59b712d92c07d67ef1e3571d730869fb13587c9
Tasman Bruce <tasmanbrucework@gmail.com>
2022-09-30 15:50:33 +1300

    Added backend config

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const firestore = admin.firestore();
+
+/**
+ * A function that is triggered when a user is created.
+ */
+export const authOnCreate =
+    functions.auth.user().onCreate(async (user) => {
+        console.log(`Creating document for user ${user.uid}`);
+        await firestore.collection("users").doc(user.uid).set({
+            createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        });
+    });
+
+/**
+ * A function that is triggered when a user is deleted.
+ */
+export const authOnDelete =
+    functions.auth.user().onDelete(async (user) => {
+        console.log(`Deleting document for user ${user.uid}`);
+        await firestore.collection("users").doc(user.uid).delete();
+    });
