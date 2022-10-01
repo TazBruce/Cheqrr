8416d1a143db027300c62e347a7d0075329373a1
Tasman Bruce <tasmanbrucework@gmail.com>
2022-10-02 02:31:28 +1300

    Basic functionality for joining teams

--- frontend/src/stores/auth.store.ts
+++ frontend/src/stores/auth.store.ts
@@ -1,11 +1,13 @@
 import { defineStore } from 'pinia';
-import { auth } from 'boot/firebase';
+import { auth, db } from 'boot/firebase';
 import {
   signOut,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
+import { doc, deleteDoc, setDoc } from 'firebase/firestore';
 import { useAuth } from '@vueuse/firebase/useAuth';
+import { useFirestore } from '@vueuse/firebase/useFirestore';
 
 const { isAuthenticated, user } = useAuth(auth);
 
@@ -14,15 +16,19 @@ const { isAuthenticated, user } = useAuth(auth);
  */
 export const useAuthStore = defineStore({
   id: 'auth',
-  state: () => ({
-    organisation: null as string | null,
-  }),
+  state: () => ({}),
   getters: {
     isLoggedIn: () => isAuthenticated.value,
     user: () => user.value,
+    organisation: () => {
+      const org = useFirestore(doc(db, 'roles', user.value?.uid as string)).value;
+      if (org) {
+        return org
+      }
+      return null;
+    }
   },
   actions: {
-
     /**
      * Registers the user.
      * @param email The email of the user
@@ -55,10 +61,12 @@ export const useAuthStore = defineStore({
         });
     },
 
+    /**
+     * Logs the user out.
+     */
     async signOut() {
       signOut(auth)
         .then(() => {
-          this.organisation = null;
           this.router.replace('/');
           alert('Signed out!');
         })
@@ -66,6 +74,46 @@ export const useAuthStore = defineStore({
           alert(error.message);
         });
     },
+
+    /**
+     * Checks firestore to see if there is an organisation with the given invite code.
+     * Joins the organisation if there is.
+     * @param inviteCode The invite code
+     */
+    async joinOrganisation(inviteCode: string) {
+      if (this.organisation != null) {
+        alert('You are already in an organisation!');
+        return;
+      }
+      const invite = await queryInviteCode(inviteCode);
+      if (invite == null || invite.organisation == null) {
+        alert('Invalid invite code!');
+        return;
+      }
+      await setDoc(doc(db, 'roles', this.user?.uid as string), {
+        orgID: invite.organisation,
+        role: 'Worker',
+      });
+      alert('Joined organisation!');
+    },
+
+    async leaveOrganisation() {
+      if (this.organisation == null) {
+        alert('You are not in an organisation!');
+        return;
+      }
+      await deleteDoc(doc(db, 'roles', this.user?.uid as string));
+      alert('Left organisation!');
+    }
   },
   persist: true,
 });
+
+/**
+ * Queries the database for a given invite code.
+ * @param inviteCode The invite code
+ */
+async function queryInviteCode(inviteCode: string) {
+  const invite = await useFirestore(doc(db, 'invite-codes', inviteCode));
+  return invite.value;
+}
