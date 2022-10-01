afe1ea8b091baace3f7b57d8c42c5c426ae9c4a8
Tasman Bruce <tasmanbrucework@gmail.com>
2022-10-02 00:28:02 +1300

    Basic layout for setup org

--- frontend/src/layouts/OutsideOrg.vue
+++ frontend/src/layouts/OutsideOrg.vue
@@ -1,14 +1,15 @@
 <template>
   <q-layout>
     <q-page-container>
-      <div class="column" style="width: 100%">
+      <div class="column" style="height: 100%">
         <div class="col-8 bg-primary">
           <h3 class="text-center text-white">Cheqrr</h3>
         </div>
-        <div class="col-4">
+        <div class="col-4 q-gutter-y-none">
           <q-btn
             @click="goBack"
             icon="chevron_left"
+            size="xl"
             flat
             round
             dense
--- frontend/src/pages/OutsideOrg/SetupOrg.vue
+++ frontend/src/pages/OutsideOrg/SetupOrg.vue
@@ -1,10 +1,26 @@
 <template>
   <q-page>
-    <h1>Setup Organization</h1>
-    <q-card>
-      <router-link to="/setup/create-organisation" class="btn btn-link" replace>Create Org</router-link>
-      <router-link to="/setup/join-organisation" class="btn btn-link" replace>Join Org</router-link>
-    </q-card>
+    <div class="column justify-center q-gutter-y-md">
+      <div class="text-center text-h5 text-bold">You're not in a team!</div>
+      <div class="text-center text-body1">You can either create a new team or join an existing one.</div>
+      <q-btn
+        to="/setup/join-organisation"
+        class="q-mx-auto"
+        size="lg"
+        label="Join Team"
+        rounded
+        color="primary"
+      />
+      <q-btn
+        to="/setup/create-organisation"
+        class="q-mx-auto"
+        size="lg"
+        label="Create Team"
+        rounded
+        outline
+        color="primary"
+      />
+    </div>
   </q-page>
 </template>
 
