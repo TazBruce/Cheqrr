04e6edfbcfd2e97c79e3c98eb0f465a78f5704d9
Tasman Bruce <tasmanbrucework@gmail.com>
2022-10-05 00:05:10 +1300

    Basic fetch implementation for items

--- frontend/src/stores/items.store.ts
+++ frontend/src/stores/items.store.ts
@@ -1,16 +1,32 @@
 import { Item } from 'src/types/Item';
 import { defineStore } from 'pinia';
+import { db } from 'boot/firebase';
+import { DocumentData, collection, getDocs, CollectionReference } from 'firebase/firestore';
+import { useAuthStore } from 'stores/auth.store';
 
 type State = {
   items: Item[]
 }
 
+const createCollection = <T = DocumentData>(collectionName: string) => {
+  return collection(db, collectionName) as CollectionReference<T>;
+}
+
 export const useItemsStore = defineStore({
   id: 'items',
   state: (): State => ({
-    items: []
+    items: [] as Item[]
   }),
   actions: {
+    async fetchItems() {
+      if (useAuthStore().organisation === null) {
+        return;
+      }
+      const itemsCollection = createCollection<Item>('organisations/' + useAuthStore().organisation + '/items');
+      this.items = (await getDocs(itemsCollection)).docs.map((doc) => doc.data());
+      console.log(this.items);
+    },
+
     /**
      * Adds an item to the store
      */
