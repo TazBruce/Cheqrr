8b9a239a7fc361f3ede500252f05d1aa4aed682c
Tasman Bruce <tasmanbrucework@gmail.com>
2022-10-07 12:42:09 +1300

    Added breadcrumb

--- frontend/src/pages/Dashboard/Items/WriteItemPage.vue
+++ frontend/src/pages/Dashboard/Items/WriteItemPage.vue
@@ -1,5 +1,15 @@
 <template>
-  <q-page>
+  <q-page class="q-gutter-y-md">
+    <q-breadcrumbs
+      class="q-mb-md"
+      :separator="`>`"
+      :separator-class="`text-grey-8`"
+      :label-class="`text-grey-8`"
+      :active-class="`text-primary`"
+    >
+      <q-breadcrumbs-el  label="Items" class="cursor-pointer q-hoverable" @click="router.back()" />
+      <q-breadcrumbs-el label="Create Item" class="text-grey-7" />
+    </q-breadcrumbs>
     <h1 class="text-h5">Create Item</h1>
     <q-form @submit="onSubmit">
       <q-input
@@ -7,14 +17,16 @@
         label="Name"
         filled
         lazy-rules
-        :rules="[val => val.length > 0 || 'Name is required']"
+        :rules="[val => val.length > 0 || 'Name is required',
+                 val => val.length <= 25 || 'Name must be less than 25 characters']"
       />
       <q-input
         v-model="description"
         label="Description"
         filled
         lazy-rules
-        :rules="[val => val.length > 0 || 'Description is required']"
+        :rules="[val => val.length > 0 || 'Description is required',
+        val => val.length < 100 || 'Description must be less than 100 characters']"
       />
       <q-btn type="submit" color="primary" label="Submit" />
     </q-form>
@@ -23,6 +35,9 @@
 
 <script setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const name = ref('');
 const description = ref('');
