4e9d50bcae8fa3fd240b4a40ae3230dd37ae7106
Tasman Bruce <tasmanbrucework@gmail.com>
2022-10-07 21:40:36 +1300

    Job alignment

--- frontend/src/components/Jobs/JobComponent.vue
+++ frontend/src/components/Jobs/JobComponent.vue
@@ -1,10 +1,10 @@
 <template>
-  <q-card class="bg-grey-3">
-    <q-card-section>
+  <q-card class="bg-grey-3 column" style="width: 200px; height: 250px;">
+    <q-card-section class="col-10">
       <div class="text-h6">{{ job.title }}</div>
       <div class="text-caption">{{ job.description }}</div>
     </q-card-section>
-    <q-card-section :class="getJobStatusColor(job.status)">
+    <q-card-section class="col" :class="getJobStatusColor(job.status)">
       <div class="text-body1 text-white">Status: {{ job.status }}</div>
     </q-card-section>
   </q-card>
