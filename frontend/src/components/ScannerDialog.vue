5c40c09afbff81b9593632be71bb8a449626fd6e
Tasman Bruce <tasmanbrucework@gmail.com>
2022-10-08 16:59:25 +1300

    Implementing QR scanning

---
+++ frontend/src/components/ScannerDialog.vue
@@ -0,0 +1,129 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide" persistent>
+    <q-card class="bg-grey-1">
+      <q-form @submit="save">
+        <q-card-section>
+          <q-input
+            v-model="scanResult"
+            label="Scanned ID"
+            :disable="true"
+            filled
+            dense
+          />
+          <div>
+            <QrStream :camera="camera" @decode="onDecode" @init="onInit">
+              <div v-if="isValid" class="validation-success">
+                This is a URL
+              </div>
+
+              <div v-if="isValid === false" class="validation-failure">
+                This is NOT a URL!
+              </div>
+
+              <div v-if="isValid === undefined && camera === 'off'" class="validation-pending">
+                Long validation in progress...
+              </div>
+            </QrStream>
+          </div>
+          <q-btn
+            class="q-mt-md"
+            color="primary"
+            label="Scan"
+          />
+          <div class="text-body1 text-center text-negative">
+            {{ error }}
+          </div>
+        </q-card-section>
+        <q-card-actions>
+          <q-btn
+            label="Close"
+            color="primary"
+            flat
+            @click="onDialogCancel"
+          />
+          <q-btn
+            type="submit"
+            label="Submit"
+            color="primary"
+            flat
+          />
+        </q-card-actions>
+        <q-inner-loading
+          :showing="loading"
+          label="Please wait..."
+          label-class="text-teal"
+          label-style="font-size: 1.1em"
+        />
+      </q-form>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { useDialogPluginComponent } from 'quasar';
+import { ref } from 'vue';
+import { QrStream } from 'vue3-qr-reader'
+
+defineEmits([
+  ...useDialogPluginComponent.emits,
+])
+
+const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } = useDialogPluginComponent()
+
+const scanResult = ref('');
+const loading = ref(false);
+const error = ref('');
+const isValid = ref(undefined);
+const camera = ref('auto');
+const result = ref(null);
+
+let image;
+
+function onInit(promise) {
+  promise
+    .catch(console.error)
+    .then(resetValidationState)
+}
+
+function resetValidationState() {
+  isValid.value = undefined
+}
+
+async function onDecode(content) {
+  scanResult.value = content;
+  result.value = content
+  turnCameraOff()
+  // pretend it's taking really long
+  await timeout(3000)
+  isValid.value = content.startsWith('http')
+  // some more delay, so users have time to read the message
+  await timeout(2000)
+  turnCameraOn()
+}
+
+function turnCameraOn() {
+  camera.value = 'auto'
+}
+
+function turnCameraOff() {
+  camera.value = 'off'
+}
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function save() {
+  if (scanResult.value === 'INVALID CODE' || scanResult.value === '') {
+    error.value = 'You must scan a valid code';
+  } else {
+    loading.value = true;
+    await onDialogOK(scanResult.value);
+    loading.value = false;
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/pages/Dashboard/Items/ItemsPage.vue
+++ frontend/src/pages/Dashboard/Items/ItemsPage.vue
@@ -11,14 +11,29 @@
     <q-page-sticky position="bottom-right" :offset="[18, 50]">
       <q-btn to="items/create" fab icon="add" color="primary" />
     </q-page-sticky>
+    <q-page-sticky position="bottom-right" :offset="[100, 50]">
+      <q-btn color="primary" fab icon="qr_code" @click="scanBarcode"/>
+    </q-page-sticky>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { useItemsStore } from 'stores/items.store';
+import ScannerDialog from 'components/ScannerDialog.vue';
 import ItemComponent from 'components/Items/ItemComponent.vue';
+import {useQuasar} from 'quasar';
 
 const { items } = useItemsStore()
+const $q = useQuasar();
+
+function scanBarcode() {
+  $q.dialog({
+    component: ScannerDialog,
+  }).onOk((result: string) => {
+    console.log(result);
+  });
+}
+
 </script>
 
 <style scoped>
